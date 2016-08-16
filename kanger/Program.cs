66c3d8a966d380b951167b1a90cdd40e5e525784
Mirage <mirageyangkang@outlook.com>
2016-08-17 00:34:29 +0800

    成功实现共享内存通信方式，基本原理

--- kanger/Program.cs
+++ kanger/Program.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace KangerToolKit
+namespace kanger
 {
-
     class Program
     {
         [DllImport("Kernel32.dll", EntryPoint = "CreateFileMapping")]
-        private static extern IntPtr CreateFileMapping(IntPtr hFile, 
+        private static extern IntPtr CreateFileMapping(IntPtr hFile,
                                                        UInt32 lpAttributes,
                                                        UInt32 flProtect,
                                                        UInt32 dwMaximumSizeHigh,
@@ -50,55 +48,20 @@ namespace KangerToolKit
         private static Semaphore m_Read;  //可读的信号
         private static IntPtr handle;     //文件句柄
         private static IntPtr addr;       //共享内存地址
-        static uint mapLength = 1024;     //共享内存长
-
+        uint mapLength = 1024;
 
         static void Main(string[] args)
         {
-            try
-            {
-                m_Write = new Semaphore(1,1,"WriteMap");
-                m_Read = new Semaphore(0,1,"ReadMap");
-                IntPtr hfile = new IntPtr(-1);
-                handle = CreateFileMapping(hfile, 0, 0x04, 0, mapLength, "shareMemory");
-                addr = MapViewOfFile(handle, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+            m_Write = Semaphore.OpenExisting("WriteMap");
+            m_Read = Semaphore.OpenExisting("ReadMap");
+            handle = OpenFileMapping(0x0002, 0, "shareMemory");
 
-                m_Write.WaitOne();
-                byte[] sendStr = Encoding.Unicode.GetBytes("还是不会izheyt12342@#￥：：“”《》<>{}[]" + '\0');
-                //如果要是超长的话，应另外处理，最好是分配足够的内存
-                if (sendStr.Length < mapLength)
-                    Copy(sendStr, addr);
-                m_Read.Release();
-
-                Console.WriteLine("write sccess");
-                Console.ReadKey();
-                
-
-                UnmapViewOfFile(addr);
-                CloseHandle(handle);
-            }
-            catch (WaitHandleCannotBeOpenedException)
-            {
-
-                Console.ReadKey();
-            }
-
-            Console.ReadKey();
-        }
-
-        static unsafe void Copy(byte[] byteSrc, IntPtr dst)
-        {
-            fixed (byte* pSrc = byteSrc)
-            {
-                byte* pDst = (byte*)dst;
-                byte* psrc = pSrc;
-                for (int i = 0; i < byteSrc.Length; i++)
-                {
-                    *pDst = *psrc;
-                    pDst++;
-                    psrc++;
-                }
-            }
+            m_Read.WaitOne();
+            IntPtr strPtr = MapViewOfFile(handle, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+            string str = Marshal.PtrToStringAuto(strPtr);
+            Console.WriteLine(str);
+            Console.ReadLine();
+            m_Write.Release();
         }
     }
 }
